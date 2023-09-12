60115bd875da444ab61c27ea1c35bc36a78ac84e
Lasse Ishøi <lasse.ishoei@gmail.com>
2023-09-12 16:06:33 +0200

    update

--- R/build.R
+++ R/build.R
@@ -1,2 +1,9 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+
+
+blogdown::build_site()
+
+blogdown::serve_site()
+
+blogdown::stop_server()
