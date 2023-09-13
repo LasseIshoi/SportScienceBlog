ac6f3ef352c5e541b48d5288521e20461d0158e4
Lasse Ishøi <lasse.ishoei@gmail.com>
2023-09-13 09:27:52 +0200

    Papers updated

--- R/Add Publication.R
+++ R/Add Publication.R
@@ -1,74 +1,68 @@
 ##Add publication##
 
-Paper_Name <- "Hip arthroscopy prediction"
+Paper_Name <- "Large eccentric strength increase using the Copenhagen Adduction exercise in football: A randomized controlled trial"
 
 
 blogdown::new_post(title = Paper_Name,  author = "Lasse Ishøi", subdir = "publication", ext = ".Rmd")
 
-#Add in .MD file
-# 
-# 
-# 
-# 
-# 
-# ---
-#   author_notes:
-#   - 
-#   - 
-#   -
-#   -
-#   -
-#   -
-#   -
-#   -
-#   authors:
-#   - 
-# - 
-# - 
-# - 
-# - 
-# - 
-# - 
-# - admin
-# date: "2022-05-09T00:00:00Z"
-# doi: ""
-# featured: false
-# image: 
-#   caption: 
-#   focal_point: ""
-# preview_only: false
-# projects: []
-# publication: ''
-# publication_short: ""
-# publication_types:
-#   - "2"
-# publishDate: "2022-05-09T00:00:00Z"
-# slides: 
-#   summary: 
-#   tags:
-#   - 
-# - 
-# - 
-# title: ""
-# url_code: ""
-# url_dataset: ""
-# url_pdf: ""
-# url_poster: ""
-# url_project: ""
-# url_slides: ""
-# url_source: 
-# url_video: ""
-# ---
-#   # Abstract
-#   
-#   **Purpose**  
-#    
-# 
-# **Methods**  
-#    
-# 
-# **Results**  
-#  
-# 
-# **Conclusion**  
-#   
+#Add in .Rmd file
+
+---
+author_notes:
+-
+-
+-
+-
+-
+-
+-
+-
+authors:
+-
+-
+-
+-
+-
+-
+-
+- admin
+date: "2022-05-09T00:00:00Z"
+doi: ""
+featured: false
+image:
+caption:
+focal_point: ""
+preview_only: false
+projects: []
+publication: ''
+publication_short: ""
+publication_types:
+  - "2"
+publishDate: "2022-05-09T00:00:00Z"
+slides:
+summary:
+tags:
+-
+-
+-
+title: ""
+url_code: ""
+url_dataset: ""
+url_pdf: ""
+url_poster: ""
+url_project: ""
+url_slides: ""
+url_source:
+url_video: ""
+---
+  **Purpose**
+  
+  
+  **Methods**
+  
+  
+  **Results**
+  
+  
+  **Conclusion**
+  
